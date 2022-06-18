d46e2c95fe077504e875a2380e0ea46827bc180f
pawan7405 <81918338+pawan7405@users.noreply.github.com>
2022-06-18 22:54:32 +0530

    done changes
    done changes

--- MymvcGitProject/Controllers/MyController.cs
+++ MymvcGitProject/Controllers/MyController.cs
@@ -5,3 +5,27 @@ Cloning into 'C:\Users\Charles\Source\Repos\FingerprintGuide'...
 Error encountered while cloning the remote repository: Git failed with a fatal error.
 repository 'https://github.com/xamarin/monodroid-samples/tree/master/FingerprintGuide/' not found
 The folder certainly exists as anyone can verify. I am signed into Git from a Web browser outside of Visual Studio. Visual Studio Git Options has my correct Git user name and e-mail.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
+Launch the “Visual Studio Installer” as shown in below fig
+Make sure if “Git for Windows” and “GitHub extension for Visual Studio” components / tools are installed.
+If not please select those and install them.
