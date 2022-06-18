06eb291699a4ce71f5c75b4c63320562c8727c38
pawan7405 <81918338+pawan7405@users.noreply.github.com>
2022-06-18 22:53:18 +0530

    chnages done
    chnages done

--- MymvcGitProject/Controllers/MyController.cs
+++ MymvcGitProject/Controllers/MyController.cs
@@ -1,5 +1,7 @@
 This is my new file
     this file is not accessible by anybody
     this is new chnages
-
-
+Cloning into 'C:\Users\Charles\Source\Repos\FingerprintGuide'...
+Error encountered while cloning the remote repository: Git failed with a fatal error.
+repository 'https://github.com/xamarin/monodroid-samples/tree/master/FingerprintGuide/' not found
+The folder certainly exists as anyone can verify. I am signed into Git from a Web browser outside of Visual Studio. Visual Studio Git Options has my correct Git user name and e-mail.
