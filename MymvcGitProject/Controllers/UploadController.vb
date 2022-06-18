7873832d7d17298fa6a3dfd46fc5fb5cf45ebaea
pawan7405 <81918338+pawan7405@users.noreply.github.com>
2022-06-18 19:54:41 +0530

    commit first

---
+++ MymvcGitProject/Controllers/UploadController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class UploadController
+        Inherits Controller
+
+        ' GET: Upload
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
